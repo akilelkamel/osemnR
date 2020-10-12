3c42a202dfc70fe1f7a82b323bdfc671bea4bf16
Akil Elkamel <akil_elkamel@yahoo.fr>
2020-10-12 11:44:51 +0300

    obtain: packages and functions to be used

--- R/obtain.R
+++ R/obtain.R
@@ -1,3 +1,22 @@
+
+# readr:  * csv:read_csv
+#         * TSV: read_tsv
+#         * delim file: read_delim
+#         * fread (data.table pkg)
+
+# readxl:  * read_excel (or gdata, XLConnect)
+
+# DBI (RMySQL, RPostgreSQL, ROracle)
+# dbConnect, dbListTables, dbReadTable, dbGetQuery, dbDisconnect
+
+# haven
+#        * SAS: read_sas()
+#        * STATA: read_stata(), read_dta()
+#        * SPSS: read_spss(), read_por(), read_sav()
+
+# foreign: SAS, STATA, SPSS, Systat, Weka
+
+# jsonlite: fromJSON()
 obtain <- function(type = c("csv", "tsv", "xls", "xlsx", "mysql"), src){
 
 }
